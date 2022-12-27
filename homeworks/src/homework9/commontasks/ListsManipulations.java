d97720d277d7225c589123bf61e51cae4472720b
Alexander Pishvanov <apishvanov@gmail.com>
2022-12-27 21:26:09 +0200

    HW9 all tasks done

---
+++ homeworks/src/homework9/commontasks/ListsManipulations.java
@@ -0,0 +1,72 @@
+package homework9.commontasks;
+
+import java.util.*;
+
+public class ListsManipulations {
+    public static void main(String[] args) {
+        Integer[] integerArray = {0, 15, 31, 0, 15, 31, 21, 18, 21, 89};
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(0, 15, 31, 0, 15, 31, 21, 18, 21, 89));
+        List<String> stringList = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "three", "two", "two"));
+
+        System.out.println(countOccurance(stringList, "two"));
+        System.out.println(toList(integerArray));
+        System.out.println(findUnique(integerList));
+        calcOccurance(stringList);
+
+        for(WordCounter elem : findOccurrence(stringList)) {
+            System.out.println(elem);
+        }
+    }
+
+    public static int countOccurance(List<String> stringList, String stringToCount) {
+        int counter = 0;
+        for (String elem : stringList) {
+            if (elem.equals(stringToCount)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static List<Integer> toList(Integer[] integerArray) {
+        List<Integer> list = new ArrayList<>();
+        for (int elem : integerArray) {
+            list.add(elem);
+        }
+        return list;
+    }
+
+    public static List<Integer> findUnique(List<Integer> integerList) {
+        return new ArrayList<>(new HashSet<>(integerList));
+    }
+
+    public static void calcOccurance(List<String> stringList) {
+        Map<String, Integer> result = new HashMap<>();
+        for (String elem : stringList) {
+            if (result.putIfAbsent(elem, 1) != null) {
+                result.put(elem, result.get(elem) + 1);
+            }
+        }
+
+        // output as required in task 4 **
+        for(String elem : result.keySet()) {
+            System.out.println(elem + ": " + result.get(elem));
+        }
+    }
+
+    public static List<WordCounter> findOccurrence(List<String> stringList) {
+        Map<String, Integer> result = new HashMap<>();
+        for (String elem : stringList) {
+            if (result.putIfAbsent(elem, 1) != null) {
+                result.put(elem, result.get(elem) + 1);
+            }
+        }
+
+        // output as required in task 4 ***
+        List<WordCounter> outputList = new ArrayList<>();
+        for(String elem : result.keySet()) {
+            outputList.add(new WordCounter(elem, result.get(elem)));
+        }
+        return outputList;
+    }
+}
---
+++ homeworks/src/homework9/commontasks/WordCounter.java
@@ -0,0 +1,18 @@
+package homework9.commontasks;
+
+public class WordCounter {
+    private String word;
+    private int occurrence;
+
+    public WordCounter (String word, int occurrence){
+        this.word = word;
+        this.occurrence = occurrence;
+    }
+
+    @Override
+    public String toString() {
+        return "{name: " + word +
+                " , occurrence: " + occurrence +
+                "}";
+    }
+}
