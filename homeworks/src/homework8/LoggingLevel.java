b20e521b7822f369531caea8cc77a1540e218557
Alexander Pishvanov <apishvanov@gmail.com>
2023-01-03 23:22:59 +0200

    comments to files added

--- homeworks/src/homework8/LoggingLevel.java
+++ homeworks/src/homework8/LoggingLevel.java
@@ -1,5 +1,13 @@
 package homework8;
 
+/**
+ * Таблиця видимості:
+ * | LEVEL | INFO | DEBUG |
+ * | INFO  |   X  |       |
+ * | DEBUG |   X  |   X   |
+ *
+ * Тобто, якщо активовано рівень DEBUG, то його також включається INFO, але не навпаки.
+ */
 public enum LoggingLevel {
     INFO,
     DEBUG;
