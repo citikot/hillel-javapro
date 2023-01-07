f1a7f4ee71e21d516d2504235106bfaeda0753c0
Alexander Pishvanov <apishvanov@gmail.com>
2023-01-08 00:20:40 +0200

    homework11 ready

---
+++ homeworks/src/homework11/coffee/order/CoffeeOrderBoardRunner.java
@@ -0,0 +1,31 @@
+package homework11.coffee.order;
+
+public class CoffeeOrderBoardRunner {
+    public static void main(String[] args) {
+        CoffeeOrderBoard orderBoard = new CoffeeOrderBoard();
+
+        orderBoard.add("Alen");
+        orderBoard.add("Alex");
+        orderBoard.add("Yongy");
+        orderBoard.add("Drax");
+        orderBoard.add("Miranda");
+        orderBoard.add("Ego");
+        orderBoard.add("Groot");
+        orderBoard.add("Stallone");
+
+        orderBoard.draw();
+
+        orderBoard.deliver();
+        orderBoard.deliver();
+        orderBoard.deliver();
+        orderBoard.deliver();
+        orderBoard.deliver();
+
+        orderBoard.draw();
+
+        orderBoard.deliver(7);
+        orderBoard.deliver(6);
+
+        orderBoard.draw();
+    }
+}
