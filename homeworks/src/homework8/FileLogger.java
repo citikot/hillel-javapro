adef1fbc53a816589095b4039941b10b83b28a18
Alexander Pishvanov <apishvanov@gmail.com>
2023-01-10 21:13:35 +0200

    hw8 done tasks 1-4

--- homeworks/src/homework8/FileLogger.java
+++ homeworks/src/homework8/FileLogger.java
@@ -1,8 +1,69 @@
 package homework8;
 
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class FileLogger {
     /*
     Клас здійснюватиме логування (протоколування) інформації
     в зазначений файл на підставі конфігураційного об'єкта.
     */
+    private FileLoggerConfiguration file;
+
+    public FileLogger(FileLoggerConfiguration file) {
+        this.file = file;
+    }
+
+    public void info(String message) throws FileMaxSizeReachedException {
+        File fo = new File(file.getFileName());
+        if ((int )fo.length() > file.maxFileSize) {
+            throw new FileMaxSizeReachedException(file.fileName, file.maxFileSize, fo);
+        }
+        DateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy '@' HH:mm:ss");
+        String date = dateFormat.format(new Date());
+        StringBuilder messageToLog = new StringBuilder();
+        messageToLog.append(date);
+        messageToLog.append(" ");
+        messageToLog.append(LoggingLevel.INFO);
+        messageToLog.append(" ");
+        messageToLog.append(" messaga: ");
+        messageToLog.append(message);
+        messageToLog.append("\n");
+
+        try (FileWriter fw = new FileWriter(fo, file.getFileFormat(), true)) {
+            fw.write(String.valueOf(messageToLog));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void debug(String message) throws FileMaxSizeReachedException {
+        File fo = new File(file.getFileName());
+        if ((int )fo.length() > file.maxFileSize) {
+            throw new FileMaxSizeReachedException(file.fileName, file.maxFileSize, fo);
+        }
+        DateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy '@' HH:mm:ss");
+        String date = dateFormat.format(new Date());
+        StringBuilder messageToLog = new StringBuilder();
+        messageToLog.append(date);
+        messageToLog.append(" ");
+        messageToLog.append(LoggingLevel.INFO);
+        messageToLog.append(" ");
+        messageToLog.append(LoggingLevel.DEBUG);
+        messageToLog.append(" ");
+        messageToLog.append(" messaga: ");
+        messageToLog.append(message);
+        messageToLog.append("\n");
+
+        try (FileWriter fw = new FileWriter(fo, file.getFileFormat(), true)) {
+            fw.write(String.valueOf(messageToLog));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- homeworks/src/homework8/FileLoggerConfiguration.java
+++ homeworks/src/homework8/FileLoggerConfiguration.java
@@ -1,5 +1,7 @@
 package homework8;
 
+import java.nio.charset.Charset;
+
 public class FileLoggerConfiguration {
     /*
     Клас представляє конфігурацію для логування.
@@ -12,6 +14,44 @@ public class FileLoggerConfiguration {
     String fileName;
     LoggingLevel loggingLevel;
     int maxFileSize;
-    String fileFormat;
+    Charset fileFormat;
 
+    public FileLoggerConfiguration(String fileName, LoggingLevel loggingLevel, int maxFileSize, Charset fileFormat) {
+        this.fileName = fileName;
+        this.loggingLevel = loggingLevel;
+        this.maxFileSize = maxFileSize;
+        this.fileFormat = fileFormat;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public LoggingLevel getLoggingLevel() {
+        return loggingLevel;
+    }
+
+    public void setLoggingLevel(LoggingLevel loggingLevel) {
+        this.loggingLevel = loggingLevel;
+    }
+
+    public int getMaxFileSize() {
+        return maxFileSize;
+    }
+
+    public void setMaxFileSize(int maxFileSize) {
+        this.maxFileSize = maxFileSize;
+    }
+
+    public Charset getFileFormat() {
+        return fileFormat;
+    }
+
+    public void setFileFormat(Charset fileFormat) {
+        this.fileFormat = fileFormat;
+    }
 }
