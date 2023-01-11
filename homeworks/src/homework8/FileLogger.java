19049d6ef79042e5973b68e611f6b636090be05d
Alexander Pishvanov <apishvanov@gmail.com>
2023-01-11 15:42:28 +0200

    fixes applied

--- homeworks/src/homework8/FileLogger.java
+++ homeworks/src/homework8/FileLogger.java
@@ -3,8 +3,6 @@ package homework8;
 import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 public class FileLogger {
@@ -12,17 +10,14 @@ public class FileLogger {
     Клас здійснюватиме логування (протоколування) інформації
     в зазначений файл на підставі конфігураційного об'єкта.
     */
-    private FileLoggerConfiguration file;
+    private FileLoggerConfiguration config;
 
     public FileLogger(FileLoggerConfiguration file) {
-        this.file = file;
+        this.config = file;
     }
 
     public void info(String message) throws FileMaxSizeReachedException {
-        File fo = new File(file.getFileName());
-        if ((int )fo.length() > file.maxFileSize) {
-            throw new FileMaxSizeReachedException(file.fileName, file.maxFileSize, fo);
-        }
+        File fo = new File(config.getFileName());
         DateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy '@' HH:mm:ss");
         String date = dateFormat.format(new Date());
         StringBuilder messageToLog = new StringBuilder();
@@ -30,11 +25,17 @@ public class FileLogger {
         messageToLog.append(" ");
         messageToLog.append(LoggingLevel.INFO);
         messageToLog.append(" ");
-        messageToLog.append(" messaga: ");
+        messageToLog.append(" message: ");
         messageToLog.append(message);
         messageToLog.append("\n");
 
-        try (FileWriter fw = new FileWriter(fo, file.getFileFormat(), true)) {
+        int messageToLogLengthInBytes = messageToLog.toString().getBytes(config.getFileFormat()).length;
+
+        if (((int )fo.length() + messageToLogLengthInBytes) > config.getMaxFileSize()) {
+            throw new FileMaxSizeReachedException(config.getFileName(), config.getMaxFileSize(), fo);
+        }
+
+        try (FileWriter fw = new FileWriter(fo, config.getFileFormat(), true)) {
             fw.write(String.valueOf(messageToLog));
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -42,10 +43,7 @@ public class FileLogger {
     }
 
     public void debug(String message) throws FileMaxSizeReachedException {
-        File fo = new File(file.getFileName());
-        if ((int )fo.length() > file.maxFileSize) {
-            throw new FileMaxSizeReachedException(file.fileName, file.maxFileSize, fo);
-        }
+        File fo = new File(config.getFileName());
         DateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy '@' HH:mm:ss");
         String date = dateFormat.format(new Date());
         StringBuilder messageToLog = new StringBuilder();
@@ -55,11 +53,11 @@ public class FileLogger {
         messageToLog.append(" ");
         messageToLog.append(LoggingLevel.DEBUG);
         messageToLog.append(" ");
-        messageToLog.append(" messaga: ");
+        messageToLog.append(" message: ");
         messageToLog.append(message);
         messageToLog.append("\n");
 
-        try (FileWriter fw = new FileWriter(fo, file.getFileFormat(), true)) {
+        try (FileWriter fw = new FileWriter(fo, config.getFileFormat(), true)) {
             fw.write(String.valueOf(messageToLog));
         } catch (IOException e) {
             throw new RuntimeException(e);
