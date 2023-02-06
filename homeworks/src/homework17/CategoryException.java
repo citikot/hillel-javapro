280b37275bd9e7689bdc8bc75c280225aa1083bd
Alexander Pishvanov <apishvanov@gmail.com>
2023-02-06 21:08:14 +0200

    hw17 basis done

---
+++ homeworks/src/homework17/CategoryException.java
@@ -0,0 +1,14 @@
+package homework17;
+
+public class CategoryException extends Throwable {
+
+    private String message;
+    public CategoryException (String category){
+        this.message = "The Product from category " + category + " is not found\n";
+    }
+
+    @Override
+    public String getMessage (){
+        return message;
+    }
+}
