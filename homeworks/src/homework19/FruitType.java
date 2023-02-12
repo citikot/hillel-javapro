062c410437e6aca6cb3be4d358e8c3237c10e010
Alexander Pishvanov <apishvanov@gmail.com>
2023-02-12 14:57:50 +0200

    hw19 done

---
+++ homeworks/src/homework19/FruitType.java
@@ -0,0 +1,22 @@
+package homework19;
+/**
+ * @author spasko
+ */
+public enum FruitType {
+    STRAWBERRY("Strawberry"),
+    APPLE("Apple"),
+    PEAR("Pear"),
+    ORANGE("Orange");
+
+    FruitType(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ homeworks/src/homework19/Vitamin.java
@@ -0,0 +1,19 @@
+package homework19;
+
+
+/**
+ * @author spasko
+ */
+public enum Vitamin {
+    A,
+    B,
+    B1,
+    B2,
+    B6,
+    B12,
+    C,
+    D,
+    K,
+    P,
+    E;
+}
