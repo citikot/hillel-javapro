d917a489065e7bb69d4dcd75c2220f9fc1681dc3
Alexander Pishvanov <apishvanov@gmail.com>
2023-03-07 17:59:22 +0200

    hw22 done

--- homework22/src/main/java/coffee/order/CoffeeOrderBoard.java
+++ homework22/src/main/java/coffee/order/CoffeeOrderBoard.java
@@ -1,11 +1,17 @@
 package coffee.order;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class CoffeeOrderBoard {
     private Queue<Order> orderBoard;
     private int counter = 1;
+
+    private static final Logger log = LoggerFactory.getLogger(CoffeeOrderBoard.class);
 
     public CoffeeOrderBoard() {
         orderBoard = new PriorityQueue<>(5, (o1, o2) -> o1.getOrderNumber() - o2.getOrderNumber());
@@ -14,20 +20,30 @@ public class CoffeeOrderBoard {
     public void add(String name) {
         Order order = new Order(counter, name);
         orderBoard.add(order);
+        log.info("Order number {} added to the queue for {}. ", counter, name);
         counter += 1;
     }
 
-    public void deliver(){
-        orderBoard.remove();
+    public void deliver() {
+        Order last;
+        try {
+            last = orderBoard.remove();
+        } catch (NoSuchElementException e) {
+            log.error("The queue is empty {}", (Object) e.getStackTrace());
+            return;
+        }
+        log.info("Order number {} delivered to {} and removed from queue. ", last.getOrderNumber(), last.getOrderOwner());
     }
 
     public void deliver(int number) {
         for (Order order : orderBoard) {
-            if (order.getOrderNumber() == number){
+            if (order.getOrderNumber() == number) {
                 orderBoard.remove(order);
+                log.info("Order number {} delivered to {} and removed from queue. ", order.getOrderNumber(), order.getOrderOwner());
                 return;
             }
         }
+        log.error("The order {} is out of range or the queue is empty. {}", number, new NoSuchElementException().getStackTrace());
     }
 
     public void draw() {
@@ -35,7 +51,7 @@ public class CoffeeOrderBoard {
         System.out.println("   Number |  Name ");
 
 
-        for (Order elem : orderBoard){
+        for (Order elem : orderBoard) {
             StringBuffer row = new StringBuffer();
             row.append("       ");
             row.append(elem.getOrderNumber());
--- homework22/src/main/java/coffee/order/CoffeeOrderBoardRunner.java
+++ homework22/src/main/java/coffee/order/CoffeeOrderBoardRunner.java
@@ -4,28 +4,24 @@ public class CoffeeOrderBoardRunner {
     public static void main(String[] args) {
         CoffeeOrderBoard orderBoard = new CoffeeOrderBoard();
 
+        orderBoard.deliver(111);
+        orderBoard.deliver();
+
         orderBoard.add("Alen");
         orderBoard.add("Alex");
         orderBoard.add("Yongy");
         orderBoard.add("Drax");
-        orderBoard.add("Miranda");
         orderBoard.add("Ego");
         orderBoard.add("Groot");
         orderBoard.add("Stallone");
-
-        orderBoard.draw();
-
         orderBoard.deliver();
         orderBoard.deliver();
         orderBoard.deliver();
         orderBoard.deliver();
         orderBoard.deliver();
-
-        orderBoard.draw();
-
+        orderBoard.deliver(3);
         orderBoard.deliver(7);
-        orderBoard.deliver(6);
-
-        orderBoard.draw();
+        orderBoard.deliver();
+        orderBoard.deliver(222);
     }
 }
