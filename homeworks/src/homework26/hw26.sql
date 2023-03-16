d2835e9fa969228f3193a10c729bfbbba8cae996
citikot <citikot@dr.com>
2023-03-17 00:45:41 +0200

    lower case fixed

--- homeworks/src/homework26/hw26.sql
+++ homeworks/src/homework26/hw26.sql
@@ -4,7 +4,7 @@ SELECT * FROM offices;
 -- Task 1
 SELECT * FROM offices WHERE officeCode IN (4,5,6,7);
 SELECT * FROM offices WHERE officeCode >= 4 AND officeCode <=7;
-SELECT * FROM offices order by officeCode LIMIT 3, 4;
+SELECT * FROM offices ORDER BY officeCode LIMIT 3, 4;
 -- Task 5
 SELECT * FROM offices ORDER BY city DESC;
 
@@ -13,7 +13,7 @@ SELECT * FROM products;
 SELECT * FROM products WHERE productName LIKE '1936%';
 -- Task 3
 SELECT * FROM products WHERE productVendor 
-	IN ( "Motor City Art Classics", "Exoto Designs") 
+	IN ("Motor City Art Classics", "Exoto Designs")
     AND productScale LIKE '1:24';
 
 SELECT * FROM orders;
