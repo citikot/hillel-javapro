865ea1d25340210eba3aff6eaa16e07537168f63
citikot <citikot@dr.com>
2023-04-19 00:51:42 +0300

    structure modified, hw31 logging removed for tomcat checking

---
+++ homework30rest/src/main/java/controller/OrdersResource.java
@@ -0,0 +1,33 @@
+package controller;
+
+import entity.OrderRepository;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/orders")
+public class OrdersResource {
+
+    OrderRepository repository = new OrderRepository();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllOrders() {
+        return "GetAllOrders Resource";
+    }
+
+//    public List<Order> getAllOrders (){
+//        return repository.getAllOrders();
+//    }
+
+    //    public String getOrderById (int id, OrderRepository repository){
+//        Order result = repository.getOrder(id);
+//        if (result != null) {
+//            return result.toString();
+//        } else {
+//            return "Not found";
+//        }
+//    }
+}
--- homework31webapptomcat/src/main/java/servlet/Hello.java
+++ homework31webapptomcat/src/main/java/servlet/Hello.java
@@ -4,25 +4,19 @@ import java.io.*;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @WebServlet(name = "servlet.Hello", urlPatterns = "/hello")
 public class Hello extends HttpServlet {
     private String message;
-    public static final Logger log = LoggerFactory.getLogger(Hello.class);
 
     @Override
     public void init() {
-        log.info("Servlet initialized.");
         message = "Hello World!";
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        log.info("Servlet doGet() method is called.");
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
@@ -32,6 +26,5 @@ public class Hello extends HttpServlet {
 
     @Override
     public void destroy() {
-        log.info("Servlet destroyed!");
     }
 }
