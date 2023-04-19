949b12c05905881b3d29bf7594837e2ddec6c7f8
citikot <citikot@dr.com>
2023-04-19 08:52:03 +0300

    last attempt to make jersey-tomcat link

--- homework30rest/src/main/java/controller/OrdersResource.java
+++ homework30rest/src/main/java/controller/OrdersResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 
-@Path("/orders")
+@Path(value = "/orders")
 public class OrdersResource {
 
     OrderRepository repository = new OrderRepository();
